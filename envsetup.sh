ea96bdd16a6c955f260548c03029707441fae62a
Andrew Dodd <atd7@cornell.edu>
2013-09-28 15:26:50 -0400

    envsetup: Set CUSTOM_BUILD environment variable
    This is used to gate a few features, including backuptool
    
    PS2 : Changes for Glade
    Change-Id: I02fee9e0765cca88c833bb0514762aa3b14642e3
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -62,6 +62,15 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+
+    if (echo -n $1 | grep -q -e "^glade_") ; then
+       GLADE_BUILD=$(echo -n $1 | sed -e 's/^glade_//g')
+    else
+       GLADE_BUILD=
+    fi
+    export GLADE_BUILD
+
+    CALLED_FROM_SETUP=true BUILD_SYSTEM=build/core \
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
